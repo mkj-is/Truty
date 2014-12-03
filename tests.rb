13891e763368cad78b8b5601151decf2b0b87a09
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2014-12-03 15:56:36 +0100

    Updated test so it returns HTML entities

--- tests.rb
+++ tests.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/ruby
 
+require 'htmlentities'
 require "./truetypo.rb"
 
 s = '"A není tu někde kousek břehu," ptal se kapitán, "kde tihle Batakové nelezou do vody?"
 Kříženec mezi Kubu a Portugalcem vrtěl hlavou. "Není, pane. Leda Devil Bay, ale to pro vás nic není."'
 
-print TrueTypo.czech(s)
+c = TrueTypo.czech(s)
+coder = HTMLEntities.new
+html = coder.encode(c, :decimal)
+print html
