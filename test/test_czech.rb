7103a7f8f92dba0bd08b4fcc521eb1a2cad9555e
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2015-01-04 16:20:08 +0100

    Dots in tests removed

--- test/test_czech.rb
+++ test/test_czech.rb
@@ -4,16 +4,16 @@ require 'test_helper'
 class TrutyTest < Test::Unit::TestCase
 
   def test_one_character_words
-    assert_equal 'S kluky v lese i u mostku a na dvoře. O prázdninách.', Truty::fix_czech_one_character_words('S kluky v lese i u mostku a na dvoře. O prázdninách.')
+    assert_equal 'S kluky v lese i u mostku a na dvoře. O prázdninách.', Truty.fix_czech_one_character_words('S kluky v lese i u mostku a na dvoře. O prázdninách.')
   end
 
   def test_long_czech_numbers
-    assert_equal '1, 12, 123, 1 234, 12 345, 123 456, 1 234 567, 12 345 678, 123 456 789, 0 123 456 789', Truty::fix_long_czech_numbers('1, 12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789, 0123456789')
+    assert_equal '1, 12, 123, 1 234, 12 345, 123 456, 1 234 567, 12 345 678, 123 456 789, 0 123 456 789', Truty.fix_long_czech_numbers('1, 12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789, 0123456789')
   end
 
   def test_czech_hyphenation_short_words
     words = 'Vstříc, FAMU, např., apod., oběd, éter, Ivan, .'
-    assert_equal words, Truty::add_soft_hyphens(words, "cs")
+    assert_equal words, Truty.add_soft_hyphens(words, "cs")
   end
 
   def test_czech_hyphenation_long_words
@@ -21,13 +21,26 @@ class TrutyTest < Test::Unit::TestCase
   end
 
   def test_czech_abbreviations
-    assert_equal 'Např. zimnice. MgA. František Nedoma, PhD. Zábava apod. Tzn. diskriminace. 1000 př. n. l.', Truty::fix_czech_abbreviations('Např. zimnice. MgA. František Nedoma, PhD. Zábava apod. Tzn. diskriminace. 1000 př. n. l.')
+    assert_equal 'Např. zimnice. MgA. František Nedoma, PhD. Zábava apod. Tzn. diskriminace. 1000 př. n. l.', Truty.fix_czech_abbreviations('Např. zimnice. MgA. František Nedoma, PhD. Zábava apod. Tzn. diskriminace. 1000 př. n. l.')
   end
 
-  def test_full
+  def test_czech_paragraph
     paragraph = 'Kdybyste hledali ostrůvek Tana Masa na mapě, našli byste jej právě na rovníku kousek na západ od Sumatry; ale kdybyste se zeptali na palubě lodi Kandong Bandoeng kapitána J. van Tocha, co to je tahle Tana Masa, před kterou právě zakotvil, nadával by chvíli a pak by vám řekl, že to je ta nejšpinavější díra v celých Sundských ostrovech, ještě mizernější než Tana Bala a přinejmenším tak zatracená jako Pini nebo Banjak; že jediný s odpuštěním člověk, který tam žije – nepočítáme-li ovšem tyhle všivé Bataky –, je opilý obchodní agent, kříženec mezi Kubu a Portugalcem a ještě větší zloděj, pohan a prase než celý Kubu a celý běloch dohromady; a že je-li na světě něco zatraceného, pak je to zatracený život na téhle zatracené Tana Masa, pane. Načež byste se ho patrně zeptali, proč tu tedy spustil ty zatracené kotvy, jako by tu chtěl zůstat zatracené tři dny; i zafuněl by podrážděně a bručel by něco v tom smyslu, že Kandong Bandoeng by sem neplula jenom pro tu zatracenou kopru nebo palmový olej, to dá rozum, a ostatně po tom vám nic není, pane, já mám své zatracené rozkazy, pane, a vy buďte tak laskav, pane, a hleďte si svého. A nadával by tak rozsáhle a hojně, jak se sluší na staršího, ale na svůj věk stále ještě svěžího lodního kapitána.'
-    assert_not_equal paragraph, Truty::fix_czech_paragraph(paragraph)
-    assert_not_nil Truty::fix_czech_paragraph(paragraph)
+    assert_not_equal paragraph, Truty.fix_czech_paragraph(paragraph)
+    assert_not_nil Truty.fix_czech_paragraph(paragraph)
+  end
+
+  def test_czech_text
+    text = '"Sorry, Captain," řekl konečně kříženec mezi Kubu a Portugalcem, "ale tady, na Tana Masa, žádné mušle nerostou. Ti špinaví Batakové," děl s nesmírnou ošklivostí, "žerou i medúzy; jsou víc ve vodě než na zemi, ženské tu smrdí rybinou, to si nemůžete představit – co jsem to chtěl říci? Aha, vy jste se ptal na ženské."
+"A není tu někde kousek břehu," ptal se kapitán, "kde tihle Batakové nelezou do vody?"
+Kříženec mezi Kubu a Portugalcem vrtěl hlavou. "Není, pane. Leda Devil Bay, ale to pro vás nic není."
+"Proč?"
+"Protože… tam nikdo nesmí, pane. Mám vám nalít, kapitáne?"
+"Thanks. Jsou tam žraloci?"
+"Žraloci a vůbec," mumlal kříženec. "Špatné místo, pane. Batakové by neradi viděli, kdyby tam někdo lezl."'
+    converted = Truty.fix_czech_text(text)
+    assert_not_equal text, converted
+    assert_equal text.count("\n"), converted.count("\n")
   end
 
 end
--- test/test_general.rb
+++ test/test_general.rb
@@ -8,33 +8,33 @@ class TrutyTest < Test::Unit::TestCase
   end
 
   def test_ellipsis
-    assert_equal "Nuda… Nuda… Ach, to je nuda…", Truty::ellipsis("Nuda... Nuda.... Ach, to je nuda......")
+    assert_equal "Nuda… Nuda… Ach, to je nuda…", Truty.ellipsis("Nuda... Nuda.... Ach, to je nuda......")
   end
 
   def test_emdash
-    assert_equal "One — or the other. Or — else. Totally — I mean it.", Truty::emdash_spaces("One — or the other. Or -- else. Totally --- I mean it.")
+    assert_equal "One — or the other. Or — else. Totally — I mean it.", Truty.emdash_spaces("One — or the other. Or -- else. Totally --- I mean it.")
   end
 
   def test_endash
-    assert_equal "One – or the other. Or – else. Totally – I mean it.", Truty::endash_spaces("One – or the other. Or - else. Totally - I mean it.")
+    assert_equal "One – or the other. Or – else. Totally – I mean it.", Truty.endash_spaces("One – or the other. Or - else. Totally - I mean it.")
   end
 
   def test_multiplication_sign
-    assert_equal '1×, 3×, 2 × 2, 10 × 10, 999 × 999, 1234 × 1234, 1 × 1000000', Truty::fix_multiplication_sign('1x, 3X, 2 x 2, 10 x 10, 999x999, 1234 X 1234, 1X1000000')
+    assert_equal '1×, 3×, 2 × 2, 10 × 10, 999 × 999, 1234 × 1234, 1 × 1000000', Truty.fix_multiplication_sign('1x, 3X, 2 x 2, 10 x 10, 999x999, 1234 X 1234, 1X1000000')
   end
 
   def test_widows
     paragraph = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     paragraph_without_widow = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    assert_equal paragraph_without_widow, Truty::fix_widows(paragraph)
+    assert_equal paragraph_without_widow, Truty.fix_widows(paragraph)
   end
 
   def test_units
-    assert_equal "50 %, § 23, # 26, * 1921, † 2000, 100 m², 10 kg, 19 ℃, 1 000 000 Kč, 250 €", Truty::fix_units("50  %, § 23, # 26, *  1921, † 2000, 100 m², 10 kg, 19 ℃, 1 000 000  Kč, 250 €")
+    assert_equal "50 %, § 23, # 26, * 1921, † 2000, 100 m², 10 kg, 19 ℃, 1 000 000 Kč, 250 €", Truty.fix_units("50  %, § 23, # 26, *  1921, † 2000, 100 m², 10 kg, 19 ℃, 1 000 000  Kč, 250 €")
   end
 
   def test_multicharacters
-    assert_equal "©℗®℠™№1©℗®℠™№2℃℉±∓", Truty::fix_multicharacters("(c)(p)(r)(sm)(tm)No.1(C)(P)(R)(Sm)(Tm)No.2°C°F+--+")
+    assert_equal "©℗®℠™№1©℗®℠™№2℃℉±∓", Truty.fix_multicharacters("(c)(p)(r)(sm)(tm)No.1(C)(P)(R)(Sm)(Tm)No.2°C°F+--+")
   end
 
   def test_brackets
@@ -42,28 +42,28 @@ class TrutyTest < Test::Unit::TestCase
   end
 
   def test_trailing_spaces
-    assert_equal "This is a test paragraph.", Truty::fix_trailing_spaces("This is a test paragraph.      ")
+    assert_equal "This is a test paragraph.", Truty.fix_trailing_spaces("This is a test paragraph.      ")
   end
 
   def test_hyphenation
     text = 'One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.'
-    assert_true text.length < Truty::add_soft_hyphens(text).length
+    assert_true text.length < Truty.add_soft_hyphens(text).length
   end
 
   def test_double_quotes
-    assert_equal '“Hello,” he said, “I come from the deep ocean.” “Pretty cool!” she answered.', Truty::fix_double_quotes('"Hello," he said, "I come from the deep ocean." "Pretty cool!" she answered.')
+    assert_equal '“Hello,” he said, “I come from the deep ocean.” “Pretty cool!” she answered.', Truty.fix_double_quotes('"Hello," he said, "I come from the deep ocean." "Pretty cool!" she answered.')
   end
 
   def test_single_quotes
-    assert_equal "\"‘Hello,’ he said, ‘I come from the deep ocean.’ ‘Pretty cool!’ she answered. And that was their story from my point of view.\"", Truty::fix_single_quotes("\"'Hello,' he said, 'I come from the deep ocean.' 'Pretty cool!' she answered. And that was their story from my point of view.\"")
+    assert_equal "\"‘Hello,’ he said, ‘I come from the deep ocean.’ ‘Pretty cool!’ she answered. And that was their story from my point of view.\"", Truty.fix_single_quotes("\"'Hello,' he said, 'I come from the deep ocean.' 'Pretty cool!' she answered. And that was their story from my point of view.\"")
   end
 
   def test_punctuation
-    assert_equal 'Hello! How are you? Boring… Still; Not: Angry. ', Truty::fix_punctuation_whitespace('Hello   ! How are you ?  Boring   …   Still ;  Not: Angry   .')
+    assert_equal 'Hello! How are you? Boring… Still; Not: Angry. ', Truty.fix_punctuation_whitespace('Hello   ! How are you ?  Boring   …   Still ;  Not: Angry   .')
   end
 
   def test_space_between_numbers
-    assert_equal '1 000 000, +420 737 87 97 17', Truty::fix_space_between_numbers('1 000 000, +420 737 87 97 17')
+    assert_equal '1 000 000, +420 737 87 97 17', Truty.fix_space_between_numbers('1 000 000, +420 737 87 97 17')
   end
 
 end
